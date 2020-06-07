905c418cad188562749a181a80070bee5f4a03db
jpd61 <jpd@dewoodyoil.com>
2020-06-07 17:39:04 -0500

    voting added

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
 
 // get all users
 router.get('/', (req, res) => {
     Post.findAll({
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+          ],
       order: [['created_at', 'DESC']],
       include: [
         {
@@ -25,7 +32,13 @@ router.get('/', (req, res) => {
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
       include: [
         {
           model: User,
@@ -46,7 +59,7 @@ router.get('/', (req, res) => {
       });
   });
 
-  router.post('/', (req, res) => {
+router.post('/', (req, res) => {
     // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
     Post.create({
       title: req.body.title,
@@ -58,9 +71,21 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
+});
+
+  // PUT /api/posts/upvote
+  router.put('/upvote', (req, res) => {
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+      .then(updatedPostData => res.json(updatedPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
   });
 
-  router.put('/:id', (req, res) => {
+
+router.put('/:id', (req, res) => {
     Post.update(
       {
         title: req.body.title
