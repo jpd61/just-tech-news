404eb0c7952ecb42986714010438cf16e500982f
jpd61 <jpd@dewoodyoil.com>
2020-06-14 15:51:56 -0500

    refactor and work on whatever is broken!

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,5 +1,6 @@
 const {format_date} = require('../utils/helpers');
 
+// creating test to that format_date() takes Date() objects and returns dates in MM/DD/YYYY
 test('format_date() returns a date string', () => {
     const date = new Date('2020-03-20 16:12:03');
   
