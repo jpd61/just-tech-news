d76f70c183d35fdbc9e20248a4eed17de47240eb
jpd61 <jpd@dewoodyoil.com>
2020-06-14 16:03:41 -0500

    removed catch from post user create

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -73,11 +73,7 @@ router.post('/', (req, res) => {
     
         res.json(dbUserData);
       });
-    })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+    });
   });
 
   // LOGIN
