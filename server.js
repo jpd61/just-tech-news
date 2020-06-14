e40445ee4515420e018168a53c93efb973ebf13a
jpd61 <jpd@dewoodyoil.com>
2020-06-14 13:10:09 -0500

    setup cookie and login logout

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -15,7 +17,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// turn on routes
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 app.use(routes);
 
 // turn on connection to db and server
